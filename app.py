7ba9331a0f3507c3e7090c8fdd2020fc099fc3fa
Himanshu Kumar Mishra <92040936+himanshu9812@users.noreply.github.com>
2025-04-24 18:24:43 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,8 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Start the Flask app on the correct port and listen on all interfaces (0.0.0.0)
+    app.run(debug=True, host='0.0.0.0', port=port)
